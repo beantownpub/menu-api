ab98f374deb7bbc62b62d830f204c452c5116902
jonny graves <jonny.graves@hqo.co>
2021-09-10 14:36:32 -0400

    fix seep_products script

--- api/manage.py
+++ api/manage.py
@@ -1,9 +1,22 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate
 from app import APP
 from database.db import db
 
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+PSQL = {
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'host': os.environ.get('DB_HOST'),
+    'db': os.environ.get('DB_NAME'),
+    'port': os.environ.get('DB_PORT')
+}
+
+database = f"postgresql://{PSQL['user']}:{PSQL['password']}@{PSQL['host']}:{PSQL['port']}/{PSQL['db']}"
+
+APP.config['SQLALCHEMY_DATABASE_URI'] = database
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(APP, db)
 manager = Manager(APP)
--- bin/seed_products.py
+++ bin/seed_products.py
@@ -8,7 +8,7 @@ from requests.auth import HTTPBasicAuth
 
 HOST = os.environ.get('API_HOST')
 PROTOCOL = os.environ.get('API_PROTOCOL', 'http')
-AUTH = HTTPBasicAuth(os.environ.get('API_USER'), os.environ.get('API_PW'))
+AUTH = HTTPBasicAuth(os.environ.get('API_USERNAME'), os.environ.get('API_PASSWORD'))
 
 
 def get_data(menu_file) -> dict:
@@ -18,25 +18,43 @@ def get_data(menu_file) -> dict:
 
 
 def post_item(menu_item):
-    url = f'{PROTOCOL}://{HOST}/v1/menu/item'
+    url = f"{PROTOCOL}://{HOST}/v1/menu/{menu_item['name']}"
     r = requests.post(url, json=menu_item, auth=AUTH)
     print(r.status_code)
 
 
 def delete_item(menu_item):
-    url = f'{PROTOCOL}://{HOST}/v1/menu/item'
-    r = requests.delete(url, json=menu_item, auth=AUTH)
+    url = f"{PROTOCOL}://{HOST}/v1/menu/{menu_item['name']}"
+    r = requests.delete(url, auth=AUTH)
     print(r.status_code)
+
+
+def create_food_items(menu):
+    sections = menu['categories'].keys()
+    for section in sections:
+        items = menu['categories'][section]
+        for i in items:
+            if i.get('location'):
+                del i['location']
+            if sys.argv[1] == 'add':
+                post_item(i)
+            else:
+                delete_item(i)
+
+def create_categories(menu):
+    categories = menu['categories'].keys()
+    for category in categories:
+        data = {
+            "name": category,
+            "is_active": True
+        }
+        url = f"{PROTOCOL}://{HOST}/v1/categories/{category}"
+        r = requests.post(url, json=data, auth=AUTH)
+        print(f"Status: {r.status_code}")
 
 
 if __name__ == '__main__':
     print(__file__)
     menu = get_data('bin/beantown_menu.json')
-    sections = menu['categories'].keys()
-    for section in sections:
-        items = menu['categories'][section]
-        for i in items:
-            if sys.argv[1] == 'add':
-                post_item(i)
-            else:
-                delete_item(i)
+    create_categories(menu)
+    create_food_items(menu)
